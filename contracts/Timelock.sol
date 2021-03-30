2af10e1948f36eaf2db54903bb9ffb9aa605a38f
pollo <pollofinancegroup@gmail.com>
2021-03-30 10:32:30 +0900

    Fix

--- contracts/Timelock.sol
+++ contracts/Timelock.sol
@@ -254,7 +254,7 @@ contract Timelock {
     event QueueTransaction(bytes32 indexed txHash, address indexed target, uint256 value, string signature, bytes data, uint256 eta);
 
     uint256 public constant GRACE_PERIOD = 14 days;
-    uint256 public constant MINIMUM_DELAY = 12 hours;
+    uint256 public constant MINIMUM_DELAY = 6 hours;
     uint256 public constant MAXIMUM_DELAY = 30 days;
 
     address public admin;
